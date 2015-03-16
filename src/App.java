c3f7576381723145d72ada8d3ce1d85b704af9e2
jc276963 <takwirira.chakaduka@my.jcu.edu.au>
2015-03-16 10:37:41 +1000

    Added Comment to App

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 /**
  * Created by jc276963 on 16/03/15.
  */
+
+/**
+ * Text viewer App - prints the contents of a string to console
+ * Created by Takwirira on 16/03/2015.
+ */
 public class App {
     public static void main(String[] args){
         String text = "This is a simple test\nOne Two Three\nHello world!";
