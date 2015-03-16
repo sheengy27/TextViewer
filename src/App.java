8bb8a2794b7391f5397d4aee03896a5cb2965656
jc276963 <takwirira.chakaduka@my.jcu.edu.au>
2015-03-16 10:34:16 +1000

    Initial Commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jc276963 on 16/03/15.
+ */
+public class App {
+    public static void main(String[] args){
+        String text = "This is a simple test\nOne Two Three\nHello world!";
+        for (int i = 0; i < text.length(); ++i){
+            System.out.println("text.charAt(i)");
+
+        }
+        System.out.println();
+    }
+}
